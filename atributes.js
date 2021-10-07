23afc9b7180ee6240be12f7c3cd4c0ab09047fc0
Misil4 <carlossilvamarques.cs@gmail.com>
2021-10-07 10:48:25 +0200

    (NEW) turn method created and enemy turn implemented

--- atributes.js
+++ atributes.js
@@ -1,18 +1,54 @@
-import { getRandomInt } from "./gameController.js";
+import {getRandomInt} from "./gameController.js";
 const initialize_hitPoints = (object) => {
     let hitPoints = object.powerstats.strength * 5
-    if (hitPoints < 300) {return hitPoints;}
-    else {return 300};
+    if (hitPoints < 300) {
+        return hitPoints;
+    } else {
+        return 300
+    };
 }
 const RandomFaces = (faces) => {
-    return getRandomInt(1,faces);
+    return getRandomInt(1, faces);
 }
 const block_Attack = (object) => {
-    let block = Math.floor((object.powerstats.speed+object.powerstats.combat)/2);
-   return RandomFaces(100)<=block ? true : false
+    let block = Math.floor((object.powerstats.speed + object.powerstats.combat) / 2);
+    let isBlocking = false;
+    return RandomFaces(100) <= block ? isBlocking = true : isBlocking = false;
 }
 const initialize_Attack = (object) => {
-    const weapon_Attack =(object.powerstats.power*object.powerstats.durability)/100;
-    return (weapon_Attack+ object.powerstats.strength) * RandomFaces(20)/100;
+    const weapon_Attack = (object.powerstats.power * object.powerstats.durability) / 100;
+    return Math.floor((weapon_Attack + object.powerstats.strength) * RandomFaces(20) / 100);
 }
-export {initialize_hitPoints,RandomFaces,block_Attack,initialize_Attack}
+const turn = (object, adversary, turn) => {
+    console.log(`Turno ${turn}\nturno de ${
+        object.name
+    }`);
+    console.log()
+    let dice = RandomFaces(100);
+    console.log(`Se lanza un dado de 100 caras y sale ${dice}`);
+    if (dice <= object.powerstats.combat) {
+        if (block_Attack(object)) {
+            console.log(`ATAQUE BLOQUEADO\ndurabilidad de ${
+                adversary.name
+            }-3 `)
+            adversary.powerstats.durability -= 3;
+            if (adversary.powerstats.durability < 1) {
+                console.log(`El jugador ${
+                    adversary.name
+                } no tiene mas durabilidad, partida finalizada`);
+            }
+        } else 
+            return true
+        
+    }
+    return false;
+}
+
+export {
+    initialize_hitPoints,
+    RandomFaces,
+    block_Attack,
+    initialize_Attack,
+    turn
+}
+
--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 import {getVillainZarate, getRandomHero} from "./gameController.js";
-import {initialize_hitPoints, RandomFaces, block_Attack, initialize_Attack} from './atributes.js';
+import {initialize_hitPoints, RandomFaces, block_Attack, initialize_Attack,turn} from './atributes.js';
 getVillainZarate().then((villainZarate) => {
     getRandomHero().then((superHero) => {
-        let turn = 0;
+        let turn_counter = 0;
+        let dice = 0;
         villainZarate.powerstats.hitpoints = initialize_hitPoints(villainZarate);
         superHero.powerstats.hitpoints = (initialize_hitPoints(superHero));
         console.log("BIENVENIDO A PANIC CHALLENGE");
-        while (villainZarate.powerstats.hitpoints > 1 || superHero.powerstats.hitpoints > 1) {
-            turn++;
+       // while (villainZarate.powerstats.hitpoints > 1 || superHero.powerstats.hitpoints > 1) {
+            turn_counter++;
+            if (villainZarate.powerstats.speed + villainZarate.powerstats.intelligence > superHero.powerstats.speed + superHero.powerstats.speed) {
+                    if (turn(villainZarate,superHero,turn_counter)) {
+                        let realAttack = initialize_Attack(villainZarate);
+                        superHero.powerstats.hitpoints-=realAttack;
+                        console.log(`${villainZarate.name} ha asestado ${realAttack} a ${superHero.name}\nVida restante ${superHero.powerstats.hitpoints}`);
+                        superHero.powerstats.durability-=1
+                        if (adversary.powerstats.durability < 1) {
+                            console.log(`El jugador ${
+                                superHero.name
+                            } no tiene mas durabilidad, partida finalizada`);
+                            return;
+                        }
+                    }
+            }
         
-        }
     })
 });
+
 
